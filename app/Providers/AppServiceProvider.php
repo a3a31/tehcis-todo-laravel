cd7d5124e6e804ea5fe3f56ccca308f4178ad32f
a3a31 <pura.vida.mae303@gmail.com>
2022-10-06 17:58:42 +0900

    test-a

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         if(\App::environment(['production']) || \App::environment(['develop'])){
-            \URL::forceScheme('https');
+            \URL::forceScheme('https'); 
         }
     }
     
